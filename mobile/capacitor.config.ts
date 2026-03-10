c06b4f55538e3c0f387d63c9d928abd4cd3fb2f3
Apurbo Banik Turjo <apurbo.turjo@therapservices.net>
2026-03-10 21:09:50 +0600

    Android build scripts added

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.screenarxiv.app',
+  appName: 'Screen Arxiv',
+  webDir: '../dist'
+};
+
+export default config;
