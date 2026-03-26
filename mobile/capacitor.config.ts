85264d3287ad90a13835e377cf963bfc59a55304
Apurbo Banik Turjo <apurbo.turjo@therapservices.net>
2026-03-26 21:09:50 +0600

    UI/UX design update

--- mobile/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.screenarxiv.app',
-  appName: 'Screen Arxiv',
-  webDir: '../dist'
-};
-
-export default config;
